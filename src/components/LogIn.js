4cdfa1e276ef03c972e0c35c549ac7ff52daa074
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-04-13 10:44:32 +0300

    login

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,38 +1,63 @@
 import React, { useState } from "react";
 import './LogIn.css';
 
-export default function Login({ setUser }) {
+function Login({ setUser }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    let handleSubmit = (e) => {
-        e.preventDefault();
-        fetch("", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ username, password }),
-        }).then((res) => {
-            if (res.ok) {
-                res.json().then((user) => setUser(user));
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch("/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (response.ok) {
+                const user = await response.json();
+                setUser(user);
+            } else {
+                throw new Error("Login failed.");
             }
-        });
-    }
+        } catch (error) {
+            // console.error(error);
+            alert("Please SignUp first or try to logIn with excisting account!")
+            // handle login error
+        }
+    };
+
     return (
         <div className="container">
-            <form className="form" onSubmit={handleSubmit}>
+            <form className="form" onSubmit={handleFormSubmit}>
                 <h1>Login</h1>
-                <label>
-                    Name:
-                    <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter Email" />
-                </label>
-                <label>
-                    Password :
-                    <input type="password" id="password" value={password}
-                        autoComplete="current-password" placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
-                </label>
+                <label htmlFor="username">Email</label>
+                <input
+                    type="text"
+                    id="username"
+                    autoComplete="off"
+                    value={username}
+                    placeholder="Enter Email"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    placeholder="Enter password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
             </form>
         </div>
-    )
+    );
 }
+
+export default Login;
+
+
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,53 +1,90 @@
 import React, { useState } from "react";
+// import './SignUp.css';
 
-export default function SignUp({ setUser }) {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+function SignUp({ setUser }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-    let handleSubmit = (e) => {
-        e.preventDefault();
-
-        fetch("", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ username, password, password_confirmation: passwordConfirmation, })
-        })
-            .then((res) => {
-                if (res.ok) {
-                    res.json().then((user) => setUser(user));
-                }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch("/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    password_confirmation: passwordConfirmation,
+                }),
             });
+            if (response.ok) {
+                const user = await response.json();
+                setUser(user);
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
+
     return (
-        <div className="banner">
-            <form onSubmit={handleSubmit}>
-                <h1>Sign Up</h1>
-                <div className="input-box">
-                    {/* <label htmlFor="username">Username</label> */}
-                    <input type="text" id="username" placeholder="Username"
-                        autoComplete="off" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <div className="signup-container">
+            <div className="banner">
+                <div className="portfolio pdx">
+                    <div className="portfolio-description pd">
+                        <h2>Welcome! <span>To Our Site</span></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Valiptatum</p>
+                        <div className="portfolio-social-icons">
+                            <a href="/"><i className="bi bi-facebook"></i></a>
+                            <a href="/"><i className="bi bi-linkedin"></i></a>
+                            <a href="/"><i className="bi bi-telegram"></i></a>
+                            <a href="/"><i className="bi bi-instagram"></i></a>
+                        </div>
+                    </div>
                 </div>
-                <div className="input-box">
-                    {/* <label htmlFor="password">Password</label> */}
-                    <input type="password" placeholder="New Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}
-                        autoComplete="current-password" />
+                <div className="login-form px">
+                    <div className="form-box">
+                        <form onSubmit={handleSubmit}>
+                            <h1>Sign Up</h1>
+                            <div className="input-box">
+                                <input
+                                    type="text"
+                                    id="username"
+                                    placeholder="Username"
+                                    autoComplete="off"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </div>
+                            <div className="input-box">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    placeholder="New Password"
+                                    autoComplete="current-password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className="input-box">
+                                <input
+                                    type="password"
+                                    id="password_confirmation"
+                                    placeholder="Password Confirmation"
+                                    autoComplete="current-password"
+                                    value={passwordConfirmation}
+                                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                                />
+                            </div>
+                            <button className="btnx" type="submit">Sign Up</button>
+                        </form>
+                    </div>
                 </div>
-                <div className="input-box">
-                    {/* <label htmlFor="password">Password Confirmation</label> */}
-                    <input
-                        type="password"
-                        placeholder="Password Confirmation<"
-                        id="password_confirmation"
-                        value={passwordConfirmation}
-                        onChange={(e) => setPasswordConfirmation(e.target.value)}
-                        autoComplete="current-password"
-                    />
-                </div>
-                <button className="btnx" type="submit">Sign Up</button>
-            </form>
+            </div>
         </div>
-    )
+    );
 }
+
+export default SignUp;
