a94a2510a8244d90825979ebd1755b22b87a41b0
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-04-19 22:35:41 +0300

    landingpage slide

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import NavBar from "./NavBar";
 import Home from "./Home";
+
 
 
 function App() {
