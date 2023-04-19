19ca41bf65074f6a456780bd3ec400ac010c2af1
brandon <brandon.ngongolo@student.moringaschool.com>
2023-04-19 12:00:24 -0400

    added bookingpage

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,10 @@
-
-import React from 'react'
-import { useState } from "react";
-import "./App.css";
-import { list, list2, list3, list4,list5, list6, list7, } from "./assets/cards-list";
-import Cards from "./components/Cards";
-import Filter from "./components/Filter";
-import Header from "./components/Header";
-
-function App() {
-  const [selectedFilter, setSelectedFilter] = useState(0);
-  return (
-    <div className="App">
-      <Header />
-      <Filter
-        selectedFilter={selectedFilter}
-        setSelectedFilter={setSelectedFilter}
-      />
-      {selectedFilter == 0 ? <Cards list={list} /> : <Cards list={list2} /> };
-      {selectedFilter == 2 ? <Cards list={list} /> : <Cards list={list3} /> };
-      {selectedFilter == 3 ? <Cards list={list} /> : <Cards list={list4} /> }
-    </div>
-=======
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import NavBar from "./NavBar";
 import Home from "./Home";
+import BookingPage from "./BookingPage";
 
 
 function App() {
@@ -43,6 +21,7 @@ function App() {
 
   return (
     <>
+    <BookingPage />
       <NavBar user={user} setUser={setUser} />
       <main>
         {user ? (
