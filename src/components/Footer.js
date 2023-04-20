dfc25fd328d38cbbaa71d996064bf53a94e39a87
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-04-20 16:03:42 +0300

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
               <div class="col-span-2">
                 <div>
                   <h2 class="text-2xl font-bold text-gray-900 light:text-white px">
-                    Leisre Space!
+                    LLeisure-space
                   </h2>
 
                   <p class="mt-4 text-gray-500 dark:text-gray-400 px">
