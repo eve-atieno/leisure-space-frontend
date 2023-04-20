4304ff155b010165d3bc8541670cdfd1b359e85c
eve-atieno <atienoevelyne53@gmail.com>
2023-04-20 16:30:22 +0300

    made changes

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
   
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={[<Home />]} />
+        <Route path="/" element={[<Home/>, <NavBar/>]} />
         <Route path="/booking" element={[<NavBar/>,<BookingPage />]} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
