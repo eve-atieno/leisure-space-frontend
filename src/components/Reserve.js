a95667c6944e04487373a62f63a5a3f21cebeace
eve-atieno <atienoevelyne53@gmail.com>
2023-04-22 23:52:42 +0300

    created review page

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,37 +1,122 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import React, { useState } from 'react';
+import DatePicker from 'react-date-picker';
 
 const Reserve = () => {
+
+const [startDate, setStartDate] = useState(new Date());
+const [endDate, setEndDate] = useState(new Date());
+
   return (
-    <>
-    <div>
-      <h1>Confirm Your Payment</h1>
-    </div>
-
-    <div>
-        <h2>Payment Details</h2>
-        <p>Card Number</p>
-        <input type="text" placeholder="Card Number" />
-        <p>Card Holder</p>
-        <input type="text" placeholder="Card Holder" />
-        <p>Expiry Date</p>
-        <input type="text" placeholder="Expiry Date" />
-        <p>CVV</p>
-        <input type="text" placeholder="CVV" />
-    </div>
     
+      <div className="flex flex-row m-4 justify-evenly">
+        <div className="">
+          <div className="flex flex-row mt-3 ml-3">
+            <Link to="/booking">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-12 h-12"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+                />
+              </svg>
+            </Link>
+            <div>
+              <h1>Confirm Your Payment</h1>
+            </div>
+          </div>
+           
+           <div className="m-9">
+          <div>
+            <h1 className="text-2xl font-bold">Your Trip</h1>
+          </div>
+          <div className="flex flex-row justify-between">
+            <h1 className="text-xl font-bold">Dates</h1>
+            <p className="font-semi-bold text-2xl" >Edit</p>
+          </div>
+          <div>
+            <p>May 12-May 20</p>
+          </div>
 
-    <div class="max-w-sm p-6 mx-4 my-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-    </a>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Read more
-        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-    </a>
-</div>
-</>
-  )
-}
+          <div className="flex flex-row justify-between">
+            <h1 className="text-xl font-bold">Guests</h1>
+            <p className="font-semi-bold text-2xl">Edit</p>
+        
+          </div>
+          <div>
+ <div>
+            <p>3 Guests, 2 infants</p>
+          </div>
+          <hr/>
+          <div className="text-center" >
+            <button className="bg-orange-400 text-white font-bold py-2 px-4 rounded-full"> Pay Now
 
-export default Reserve
+            </button>
+          </div>
+        </div>
+         </div>
+        <div className="max-w-lg p-6 pr-5 mx-4 my-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex flex-row ">
+            <div>
+              <img
+                className="object-cover w-40 h-40 rounded"
+                src="https://a0.muscache.com/im/pictures/94939489/6e77d654_original.jpg?im_w=1440"
+                alt="avatar"
+              />
+            </div>
+            <div className="flex flex-col ml-4">
+              <div>
+                <h3> The Villa</h3>
+              </div>
+              <div>
+                <h5> 3 Bedrooms</h5>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <p className="text-xl">
+              Your Booking Is Secure by
+              <span className="font-bold text-orange-400">LeisureSpace</span>
+            </p>
+          </div>
+          <hr />
+          
+          <div>
+            <h5 className="font-bold">Price Details</h5>
+          </div>
+          <div className="flex flex-row justify-between">
+            <h5>ksh 3000/per night*7</h5>
+            <h5>ksh 21000</h5>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between">
+            <h5 className="font-bold">Total</h5>
+            <h5>ksh 21000</h5>
+          </div>
+        </div>
+        </div>
+              <DatePicker
+        value={startDate}
+        onChange={date => setStartDate(date)}
+      />
+      <DatePicker
+        value={endDate}
+        onChange={date => setEndDate(date)}
+      />
+         
+        </div>
+
+    );
+};
+
+export default Reserve;
