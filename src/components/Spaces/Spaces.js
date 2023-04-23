970b3a61ded3c03d6421b25c1d9b77a7f67f5608
wanjirugachunji <wanjirugachunji@gmail.com>
2023-04-23 12:16:56 +0300

    Spaces updated

---
+++ src/components/Spaces/Spaces.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from "react";
+//import "../App.css";
+//import { list, list2 } from "../assets/cards-list";
+import Cards from "../Cards/Cards";
+import Filter from "../Filter";
+
+
+function Spaces (){
+  const [selectedFilter, setSelectedFilter] = useState(0);
+    return (
+     
+        <div className="categories">
+          
+          <Filter
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
+      />
+      <Cards/>
+      
+    </div>
+   );
+ }
+ 
+
+export default Spaces;
